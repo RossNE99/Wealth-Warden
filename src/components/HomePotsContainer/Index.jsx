dfdf95c6d466d7d15412311b1f4d62911cfd8725
Ross <rossycharlton@googlemail.com>
2024-03-16 21:30:35 +0000

    Added graph Component and is displaying mock data

--- src/components/HomePotsContainer/Index.jsx
+++ src/components/HomePotsContainer/Index.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import HomePot from '../HomePot/Index'
 
 function HomePotsContainer() {
-  const moc =[{name: "test1", ammountInInPot: 10, totalAllocated: 20, type:"save"}, {name: "test2", ammountInInPot: 2, totalAllocated: 10, type:"save"}, {name: "test3", ammountInInPot: 2, totalAllocated: 10,type:"spend"}]
+  const moc =[{name: "test1", ammountInInPot: 10, totalAllocated: 20, type:"save"}, 
+  {name: "test2", ammountInInPot: 2, totalAllocated: 10, type:"save"},
+   {name: "test3", ammountInInPot: 2, totalAllocated: 10,type:"spend"},
+    {name: "test1", ammountInInPot: 10, totalAllocated: 20, type:"save"},
+     {name: "test2", ammountInInPot: 2, totalAllocated: 10, type:"save"},
+      {name: "test3", ammountInInPot: 2, totalAllocated: 10,type:"spend"},
+       {name: "test1", ammountInInPot: 10, totalAllocated: 20, type:"save"},
+        {name: "test2", ammountInInPot: 2, totalAllocated: 10, type:"save"},
+         {name: "test3", ammountInInPot: 2, totalAllocated: 10,type:"spend"}, ]
   return (
-    <div className='mt-5 md:col-span-6 sm:col-span-1 shadow-lg rounded-lg mb-auto m-2'>
-      <h2 className='text-2xl font-semibold'>Pots</h2>
+    <div className='md:col-span-6 sm:col-span-1 shadow-lg rounded-lg mb-auto m-2 md:h-5/6 mt-auto pb-3 overflow-hidden'>
+    <h2 className='text-2xl font-semibold m-3'>Pots</h2>
+    <div className='overflow-y-auto max-h-full'>
       <div className='flex flex-wrap justify-evenly'>
         {moc.map((pot) => (
           <HomePot key={pot.name} {...pot} />
         ))}
       </div>
     </div>
+  </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import HomePotsContainer from '../components/HomePotsContainer/Index'
 import Log from '../components/Log/Index'
+import Graph from '../components/Graph/Index'
 
 function Home() {
   return (
-    <div className='grid md:grid-cols-10 sm:grid-cols-1 gap-1 container mx-auto'>
+    <div className='grid md:grid-cols-10 sm:grid-cols-1 gap-1 container mx-auto md:pr-4'>
       <HomePotsContainer/>
-      <div className='md:col-span-4 sm:col-span-1'>
+      <div className='shadow-lg rounded-lg p-3 md:col-span-4 sm:col-span-1 md:h-5/6 mt-auto mb-auto'>
         <Log size={"md:h-1/2"}/>
+        <Graph/>
       </div>
     </div>
   )
