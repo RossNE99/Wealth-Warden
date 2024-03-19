7b69b4d104a50c6bf8260ceb1e4c0ee6d4e30704
Temitope <topsinade@gmail.com>
2024-03-19 22:33:38 +0000

    Logo added to the Navbar

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -8,9 +8,10 @@ import { AiOutlineAreaChart } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Button } from 'flowbite-react';
 import Date from '../Date';
+import Logo from '../../assets/Logo.jpg';
 
 
-function Navbar({children}) {
+function Navbar({ children }) {
   const [toggled, setToggled] = useState(false);
   const [broken, setBroken] = useState(window.matchMedia('(max-width: 764px)').matches);
 
@@ -18,36 +19,40 @@ function Navbar({children}) {
 
   return (
     <>
-     {broken && <Button gradientDuoTone="purpleToBlue" className=" text-white font-bold m-3 p-2 rounded" onClick={() => setToggled(!toggled)}><GiHamburgerMenu /></Button>}
-    <div className='flex' style={{ height, minHeight: '400px' }}>
-    <Sidebar 
-    toggled={toggled} 
-    customBreakPoint="764px" 
-    onBreakPoint={setBroken}
-    onBackdropClick={() => setToggled(false)}
-    transitionDuration={1000}
-    >
-      <Menu
-        menuItemStyles={{
-          button: {
-            // the active class will be added automatically by react router
-            // so we can use it to style the active menu item
-            [`&.active`]: {
-              backgroundColor: '#13395e',
-              color: '#b6c8d9',
-            },
-          },
-        }}
-      >
-        <h2 className='text-3xl font-bold m-3 mb-5 bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-indigo-500'>Wealth-Warden</h2>
-        <MenuItem component={<Link to="/Home" />} icon={<FaHome />}> Home</MenuItem>
-        <MenuItem component={<Link to="/Budgeting" />} icon={<AiOutlineAreaChart/>}> Budgeting</MenuItem>
-        <MenuItem component={<Link to="/Statement" />} icon={<RiBillFill />}> Statement</MenuItem>
-      </Menu>
-      <Date/>
-    </Sidebar>
-    {children}
-    </div>
+      {broken && <Button gradientDuoTone="purpleToBlue" className=" text-white font-bold m-3 p-2 rounded" onClick={() => setToggled(!toggled)}><GiHamburgerMenu /></Button>}
+      <div className='flex' style={{ height, minHeight: '400px' }}>
+        <Sidebar
+          toggled={toggled}
+          customBreakPoint="764px"
+          onBreakPoint={setBroken}
+          onBackdropClick={() => setToggled(false)}
+          transitionDuration={1000}
+        >
+          <Menu
+            menuItemStyles={{
+              button: {
+                // the active class will be added automatically by react router
+                // so we can use it to style the active menu item
+                [`&.active`]: {
+                  backgroundColor: '#13395e',
+                  color: '#b6c8d9',
+                },
+              },
+            }}
+          >
+            <h2 className='text-3xl font-bold m-3 mb-5 bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-indigo-500'>Wealth-Warden</h2>
+            <div className="navbar-logo">
+              <img src={Logo} alt="Logo" style={{ width: '100', height: 'auto' }} />
+            </div>
+            {/* <img src={logo} alt="logo" /> */}
+            <MenuItem component={<Link to="/Home" />} icon={<FaHome />}> Home</MenuItem>
+            <MenuItem component={<Link to="/Budgeting" />} icon={<AiOutlineAreaChart />}> Budgeting</MenuItem>
+            <MenuItem component={<Link to="/Statement" />} icon={<RiBillFill />}> Statement</MenuItem>
+          </Menu>
+          <Date />
+        </Sidebar>
+        {children}
+      </div>
     </>
   )
 }
