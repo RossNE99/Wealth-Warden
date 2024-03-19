1875f019c8174b04f46788cb0d8216628f10647c
Eitel <eiteltchapdeu@gmail.com>
2024-03-19 20:44:17 +0000

    Added the Date component to the Navbar

--- src/components/Date/Index.jsx
+++ src/components/Date/Index.jsx
@@ -16,22 +16,35 @@ function Date() {
   };
 
   return (
-    <div>
-      <select value={selectedDate.month()} onChange={handleMonthChange}>
-        {Array.from({ length: 12 }, (_, i) => i).map((month) => (
-          <option key={month} value={month}>
-            {dayjs().month(month).format("MMMM")}
-          </option>
-        ))}
-      </select>
-      <select value={selectedDate.year()} onChange={handleYearChange}>
-        {/* To begin array from year 2024 and span 10years */}
-        {Array.from({ length: 10 }, (_, i) => i + 2024).map((year) => (
-          <option key={year} value={year}>
-            {year}
-          </option>
-        ))}
-      </select>
+    <div className="flex items-center flex-wrap justify-center space-x-4 mb-5 mt-5">
+      <div>
+        <h2>Current Month</h2>
+      </div>
+      <div>
+        <select
+          value={selectedDate.month()}
+          onChange={handleMonthChange}
+          className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        >
+          {Array.from({ length: 12 }, (_, i) => i).map((month) => (
+            <option key={month} value={month} className="capitalize">
+              {dayjs().month(month).format("MMMM")}
+            </option>
+          ))}
+        </select>
+        <select
+          value={selectedDate.year()}
+          onChange={handleYearChange}
+          className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        >
+          {/* To begin array from year 2024 and span 10 years */}
+          {Array.from({ length: 10 }, (_, i) => i + 2024).map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
