b00b5eb6c30dad2d788fe19645e1fc4d5a0dc7dd
Eitel <eiteltchapdeu@gmail.com>
2024-03-20 23:33:40 +0000

    Changed the modals button colors and fixed the log and graph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ function Home() {
   return (
     <div className='grid md:grid-cols-10 sm:grid-cols-1 gap-1 container mx-auto md:pr-4'>
       <HomePotsContainer/>
-      <div className='shadow-lg rounded-lg p-3 md:col-span-4 sm:col-span-1 md:h-5/6 mt-auto mb-auto'>
-        <Log size={"h-3/6"}/>
+      <div className='flex flex-col shadow-lg rounded-lg p-3 md:col-span-4 sm:col-span-1 md:h-5/6 mt-auto mb-auto'>
+        <Log size={""}/>
         <Graph/>
       </div>
     </div>
